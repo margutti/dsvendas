ce0d392672a2b94a68cf6d1dbc791f4ea0248a66
Sergio Roberto Margutti <margutti@softmidia.com.br>
2021-05-03 21:54:22 -0300

    Project created

---
+++ backend/src/test/java/br/com/softmidia/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softmidia.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
