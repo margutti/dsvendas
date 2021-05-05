5d400dd6276a411c877b499e42b6aba5ae36d47e
Sergio Roberto Margutti <margutti@softmidia.com.br>
2021-05-05 11:28:53 -0300

    Layers

---
+++ backend/src/main/java/br/com/softmidia/dsvendas/services/SellerService.java
@@ -0,0 +1,24 @@
+package br.com.softmidia.dsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.softmidia.dsvendas.dto.SellerDTO;
+import br.com.softmidia.dsvendas.entities.Seller;
+import br.com.softmidia.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result =  repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+
+}
